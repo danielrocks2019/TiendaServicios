df82a9bf4abddb8a77d4bbbdb3e68b58c819cd9c
amilkar miguel <amcmiki@gmail.com>
2019-06-20 23:51:28 -0400

    habilitacion de puerto

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = 8000;
+app.listen(port,() =>{
+  console.log("RUNING SERVER IN THE PORT " + port);
+});
 module.exports = app;
