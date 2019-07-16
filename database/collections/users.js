3d5d519b6e94e118e6bd1ff34f29b33898c31039
amilkar miguel <amcmiki@gmail.com>
2019-07-16 17:37:13 -0400

    new services modify

---
+++ database/collections/users.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = Schema({
+    name: String,
+    lastname:String,
+    phone:String,
+    email: String,
+    password: String,
+    lat: String,
+    lon: String,
+    signupDate: {type:Date, default:Date.now()}
+})
+
+
+module.exports = mongoose.model('User', UserSchema)
